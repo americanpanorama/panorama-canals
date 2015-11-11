deeaee5b6aa4c20b4e6212c43bf27d4760e25d25
Eric Socolofsky <eric@transmote.com>
2015-11-10 16:31:23 -0800

    Implement scroll arrow functionality

--- src/components/ItemSelector/ItemSelector.jsx
+++ src/components/ItemSelector/ItemSelector.jsx
@@ -74,13 +74,33 @@ export default class ItemSelector extends React.Component {
 		}
 		if (!dir) { return; }
 
-		console.log(">>>>> TODO: scroll dir:", dir);
+		let itemList = this.refs['item-list'],
+			nextAccelCounter = 16,
+			accelCounter = 0,
+			itemEl = itemList.querySelector('li'),
+			itemMetrics = window.getComputedStyle(itemEl),
+			speed = itemEl.offsetHeight + (itemMetrics ? parseFloat(itemMetrics['margin-bottom'].replace('px', '')) : 0);
+
+		this.arrowMouseUp = false;
+
+		let onArrowMouseHold = function () {
+			if (accelCounter-- <= 0) {
+				itemList.scrollTop += dir * speed;
+				accelCounter = nextAccelCounter = Math.max(1, Math.floor(nextAccelCounter * 0.75));
+			}
+			
+			if (!this.arrowMouseUp) {
+				window.requestAnimationFrame(onArrowMouseHold);
+			}
+		}.bind(this);
+
+		window.requestAnimationFrame(onArrowMouseHold);
 
 	}
 
 	onArrowMouseUp (event) {
 
-		console.log(">>>>> arrow mouse up");
+		this.arrowMouseUp = true;
 		
 	}
 
@@ -96,20 +116,20 @@ export default class ItemSelector extends React.Component {
 			<div className='panorama item-selector'>
 				<h3>SELECT A CANAL:</h3>
 				<div className='scroll-arrow up-arrow' onMouseDown={ this.onArrowMouseDown } onMouseUp={ this.onArrowMouseUp } />
-				<ul>
-				{ Object.keys(this.props.items).map((itemKey, i) => {
-					let item = this.props.items[itemKey];
-					return (
-						<li
-							className = { 'item' + (this.props.selectedItem.id === item.id ? ' selected' : '') }
-							data-item = { itemKey }
-							key = { i }
-							onClick = { this.onItemClick }
-						>
-							<span>{ item.name.toUpperCase() }</span>
-						</li>
-					);
-				}) }
+				<ul ref='item-list'>
+					{ Object.keys(this.props.items).map((itemKey, i) => {
+						let item = this.props.items[itemKey];
+						return (
+							<li
+								className = { 'item' + (this.props.selectedItem.id === item.id ? ' selected' : '') }
+								data-item = { itemKey }
+								key = { i }
+								onClick = { this.onItemClick }
+							>
+								<span>{ item.name.toUpperCase() }</span>
+							</li>
+						);
+					}) }
 				</ul>
 				<div className='scroll-arrow down-arrow' onMouseDown={ this.onArrowMouseDown } onMouseUp={ this.onArrowMouseUp } />
 			</div>
